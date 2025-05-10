bd4e367efccdc493ef9551f5aaf0963b3121cccb
Guilherme Ramos Campestrini <gramoscampestrini@gmail.com>
2025-05-10 02:05:30 -0300

    funcoes e estilos adicionados

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('#textoTarefa').focus();
-const tarefa = document.querySelector('#lista');
+const tarefa = document.querySelector('#listaItens');
 const item = [];
 const botao2 = document.querySelector('#botaoLimpar')
 
@@ -19,21 +19,49 @@ function adiciona() {
     const itemLista = document.createElement('li');
 
     const label = document.createElement('label');
-    label.style.display = 'flex';
-    label.style.alignItems = 'center';
-    label.style.flexWrap = 'nowrap';
-    label.style.gap = '5px'
+    label.classList.add('label');
 
     const checkbox = document.createElement('input');
     checkbox.type = "checkbox";
-    checkbox.style.verticalAlign = 'middle';
+    checkbox.classList.add('checkbox');
+
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            label.classList.add('checked');
+            botaoRemover.classList.add('checked');
+            label.style.transition = 'none';
+            botaoRemover.style.transition = 'none';
+            itemLista.style.color = 'white';
+            botaoRemover.style.color = 'white';
+        } else {
+            label.classList.remove('checked');
+            botaoRemover.classList.remove('checked');
+            label.style.transition = 'background-color ease-in-out 0.3s';
+            botaoRemover.style.transition = 'background-color ease-in-out 0.3s';
+            itemLista.style.color = 'black';
+            botaoRemover.style.color = 'black';
+        }
+    })
 
     const textoNode = document.createTextNode(texto);
+
+    const botaoRemover = document.createElement('button');
+    botaoRemover.innerText = 'delete'
+    botaoRemover.classList.add('material-symbols-outlined', 'botaoRemover');
+    botaoRemover.style.verticalAlign = 'middle';
+
+    botaoRemover.onclick = function() {
+        this.parentElement.parentElement.remove();
+        if (listaItens.childElementCount == 0) {
+            botao2.style.display = 'none';
+        }
+        }
 
     label.appendChild(checkbox);
     label.appendChild(textoNode)
     itemLista.appendChild(label);
     tarefa.appendChild(itemLista);
+    label.appendChild(botaoRemover);
 
     document.querySelector('#textoTarefa').value = "";
     document.querySelector('#textoTarefa').focus();
